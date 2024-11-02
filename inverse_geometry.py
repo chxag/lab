926da44bfa4bcca3b1276d71fd8a4f219b451b2b
HJulie11 <115992979+HJulie11@users.noreply.github.com>
2024-11-02 13:43:52 +0000

    Update inverse_geometry.py

--- inverse_geometry.py
+++ inverse_geometry.py
@@ -19,8 +19,49 @@ def computeqgrasppose(robot, qcurrent, cube, cubetarget, viz=None):
     '''Return a collision free configuration grasping a cube at a specific location and a success flag'''
     setcubeplacement(robot, cube, cubetarget)
     #TODO implement
-    print ("TODO: implement me")
-    return robot.q0, False
+    for i in range(300):
+        pin.framesForwardKinematics(robot.model, robot.data, qcurrent)
+        pin.computeJointJacobians(robot.model, robot.data, qcurrent)
+
+        left_hand = robot.model.getFrameId(LEFT_HAND)
+        right_hand = robot.model.getFrameId(RIGHT_HAND)
+        left_hook = robot.model.getFrameId(LEFT_HOOK)
+        right_hook = robot.model.getFrameId(RIGHT_HOOK)
+
+    #     print(left_hook, right_hook, left_hand, right_hand)
+
+        oMleft_hand = robot.data.oMf[left_hand]
+        oMright_hand = robot.data.oMf[right_hand]
+
+    #     print(oMleft_hand, oMright_hand)
+
+        oMleft_hook = getcubeplacement(cube, LEFT_HOOK)
+        oMright_hook = getcubeplacement(cube, RIGHT_HOOK)
+
+        error_left = oMleft_hand.inverse()*oMleft_hook
+        error_right = oMright_hand.inverse()*oMright_hook
+
+        error_left_vec = pin.log(error_left).vector
+        error_right_vec = pin.log(error_right).vector
+
+        error_both = np.vstack((error_left_vec, error_right_vec))
+        error_both = error_both.reshape(-1)
+
+        Jleft = pin.computeFrameJacobian(robot.model, robot.data, qcurrent, left_hand)
+        Jright = pin.computeFrameJacobian(robot.model, robot.data, qcurrent, right_hand)
+
+        Jtotal = np.vstack((Jleft, Jright))
+
+        v_q = pinv(Jtotal)@error_both
+
+#         if not collision(robot, qcurrent):
+        qcurrent = pin.integrate(robot.model, qcurrent, v_q * 1e-2)
+        updatevisuals(viz, robot, cube, qcurrent)
+    
+#     print ("TODO: implement me")
+    return qcurrent, collision(robot, qcurrent)
+    # distance between qcurrent and the goal < epsilon
+#     return robot.q0, False
             
 if __name__ == "__main__":
     from tools import setupwithmeshcat
