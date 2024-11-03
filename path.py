43663ab728665e6a0e0feb464dec767c3cd00ac9
Charvi Agarwal <charviagarwal2002@gmail.com>
2024-11-03 14:32:57 +0000

    still dunno

--- path.py
+++ path.py
@@ -117,7 +117,7 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
         while node[0] is not None:
             path = [node[1]] + path
             node = G[node[0]]
-        path = G[0][1] + path
+        path = [G[0][1]] + path
         return path
                                              
 
