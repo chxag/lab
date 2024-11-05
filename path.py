70601813013c8a42c0160f70eac9a153b71a8b14
HJulie11 <115992979+HJulie11@users.noreply.github.com>
2024-11-05 13:14:19 +0000

    Update path.py

--- path.py
+++ path.py
@@ -9,9 +9,13 @@ Created on Thu Sep 21 11:44:32 2023
 import pinocchio as pin
 import numpy as np
 from numpy.linalg import pinv
+# from setup_meshcat import updatevisuals
 
 from config import LEFT_HAND, RIGHT_HAND
+from tools import collision, getcubeplacement, setcubeplacement, projecttojointlimits
 import time
+
+
 
 #returns a collision free path from qinit to qgoal under grasping constraints
 #the path is expressed as a list of configurations
@@ -24,11 +28,14 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
     min_dist = 10e4
     idx = -1
     
-    G = [(None,np.array(qinit))]
+    G = [(None,np.array(cubeplacementq0))]
+#     T = [(None, np.array(cubeplacementq0))]
+#     cube_path = [cubeplacementq0]
     
     rotation = cubeplacementq0.rotation
     translation = cubeplacementq0.translation
     translation_goal = cubeplacementqgoal.translation
+    
     cube_q_target = pin.SE3(cubeplacementqgoal.rotation, translation_goal)
     
     sampled_positions = set()
@@ -36,7 +43,7 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
 
     x_range = (translation[0], translation_goal[0])
     y_range = (translation[1], translation_goal[1])
-    z_range = (translation[2], translation[2]+0.3)
+    z_range = (translation[2], translation[2] + 0.1)
     
     for iteration in range(k):
         
@@ -58,13 +65,15 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
                 print(f"Sampled configuration: {q_rand}, success: {success}")
             
                 if not success:
+#                     T+=[(cube, np.array(cube_q_rand))]
+#                     cube_path.append(cube_q_rand)
                     break
                 else: 
                     # Change the range of the sampling space 
                     x_range = (min(0, cube_x_rand - 0.1), max(0.5, cube_x_rand + 0.1))
                     y_range = (min(-0.2, cube_y_rand - 0.1), max(0.2, cube_y_rand + 0.1))   
                     z_range = (min(0.93, cube_z_rand - 0.1), max(1.2, cube_z_rand + 0.1))
-            
+        
         # Find the nearest vertex to q_rand called q_near 
         for i, (parent,node) in enumerate(G):
             dist = np.linalg.norm(q_rand - node[1])
@@ -85,10 +94,10 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
             dist_two = delta_q  
         dt = dist_two / discretisationsteps_newconf
         for i in range(1, discretisationsteps_newconf):
-            q_new = q_near * (1 - dt*i) + q_end * (dt*i)
+            q_new = q_near * (1 - dt*i/discretisationsteps_newconf) + q_end * (dt*i/discretisationsteps_newconf)
             q_newt, success = computeqgrasppose(robot, qinit, cube, cube_q_rand, viz)
             if success:
-                q_new = q_near * (1 - dt*(i-1)) + q_end * (dt*(i-1))
+                q_new = q_near * (1 - dt*(i-1)/discretisationsteps_newconf) + q_end * (dt*(i-1)/discretisationsteps_newconf)
             else:
                 q_new = q_end
         q_new = np.array(q_new)
@@ -121,21 +130,48 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
 
     # Reconstruct the path from qinit to qgoal
     path = []
+    cube_path = []
     node = G[-1]
     while node[0] is not None:
         path = [node[1]] + path
+        #get the cube configuration based on the robot configuration at this path step
+        q_path, success_path = computeqgrasppose(robot, qinit, cube, cube_q_target,viz)
+        if success_path:
+            cube_path.insert(0, getcubeplacement(cube))
         node = G[node[0]]
     path = [G[0][1]] + path
-    q_last, success_last = computeqgrasppose(robot,qinit,cube,cube_q_target,viz)
-    print(path)
-    return path
-                                             
+    cube_path.insert(0,cubeplacementq0)
+    
+    print("robot path: ", path)
+    print("cube path: ", cube_path)
+    return path, cube_path
 
-def displaypath(robot,path,dt,viz):
-    for q in path:
-        viz.display(q)
-        time.sleep(dt)
+def lerp(q0,q1,t):    
+    return q0 * (1 - t) + q1 * t 
 
+def displayedge (q0, q1, cube_q0, cube_q1, vel=2.):
+    from math import ceil
+    from time import sleep
+    from setup_meshcat import updatevisuals
+    dist = np.linalg.norm(q1-q0)
+    duration = dist / vel
+    nframes = ceil(48. * duration)
+    f = 1. / 48.
+    for i in range(nframes-1):
+        interp_q = lerp(q0, q1, float(i)/nframes)
+        interp_cube_q = lerp(cube_q0.translation, cube_q1.translation, float(i)/nframes)
+        viz.display(interp_q)
+        setcubeplacement(robot, cube, cube_q0)
+        updatevisuals(viz, robot, cube, interp_q)
+        sleep(f)
+    viz.display(q1)
+    setcubeplacement(robot, cube, cube_q1)
+    updatevisuals(viz, robot, cube, q1)
+    sleep(f)
+                                            
+def displaypath(robot,path,cube_path,dt,viz):
+    for (q0, q1), (cube_q0, cube_q1) in zip(zip(path[:-1],path[1:]), zip(cube_path[:-1], cube_path[1:])):
+        displayedge(q0,q1, cube_q0, cube_q1)
 
 if __name__ == "__main__":
     from tools import setupwithmeshcat
@@ -152,7 +188,7 @@ if __name__ == "__main__":
     if not(successinit and successend):
         print ("error: invalid initial or end configuration")
     
-    path = computepath(q0,qe,CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
+    path, cube_path = computepath(q0,qe,CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
     
-    displaypath(robot,path,dt=0.5,viz=viz) #you ll probably want to lower dt
+    displaypath(robot,path,cube_path, dt=0.5,viz=viz) #you ll probably want to lower dt
     
