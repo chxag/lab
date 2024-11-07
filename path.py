fb6439cdbb7082d644cab74a03bbea7eaeaf67e6
Charvi Agarwal <charviagarwal2002@gmail.com>
2024-11-07 10:03:50 +0000

    trying stuff

--- path.py
+++ path.py
@@ -12,7 +12,93 @@ from numpy.linalg import pinv
 from tools import setcubeplacement
 
 from config import LEFT_HAND, RIGHT_HAND
+from tools import collision, getcubeplacement, setcubeplacement, projecttojointlimits, distanceToObstacle
+from config import EPSILON
 import time
+
+def coll(q):
+    return distanceToObstacle(robot, q) > 0
+
+def cube_collision(oMf):
+    setcubeplacement(robot, cube, oMf)
+    dist_obst = pin.computeDistance(cube.collision_model, cube.collision_data, 1).min_distance
+    return dist_obst < 0.02
+
+def robot_collision(q):
+    pin.updateGeometryPlacements(robot.model,robot.data,robot.collision_model,robot.collision_data,q)
+    return pin.computeCollisions(robot.collision_model,robot.collision_data,False)
+
+def distance(q1, q2):
+    return np.linalg.norm(q2.translation - q1.translation)
+
+def distance_robot(q1, q2):
+    return np.linalg.norm(q2 - q1)
+
+
+def NEAREST_VERTEX_ROBOT_Q(G,q_rand):
+    min_dist = 10e4
+    idx=-1
+    for i, (parent, cube_q, robot_q) in enumerate(G):
+        dist = distance_robot(robot_q,q_rand)
+        if dist < min_dist:
+            min_dist = dist
+            idx = i
+    return idx
+
+def NEAREST_VERTEX_CUBE_Q(G,cube_q_rand):
+    min_dist = 10e4
+    idx=-1
+    for i, (parent, cube_q, robot_q) in enumerate(G):
+        cube_q = pin.SE3(cube_q)
+        dist = distance(cube_q,cube_q_rand)
+        if dist < min_dist:
+            min_dist = dist
+            idx = i
+    return idx
+
+def ADD_EDGE_AND_VERTEX(G,parent, cube_placement, robot_q):
+    G += [(parent, cube_placement, robot_q)]
+    
+def lerp(q0,q1,t):    
+    return q0 * (1 - t) + q1 * t 
+
+def NEW_CONF_CUBE(q_near, q_rand, discretisationsteps, delta_q=None):
+    q_end = q_rand.copy()
+    dist = distance(q_near, q_rand)
+    if delta_q is not None and dist > delta_q:
+        q_end = lerp(q_near, q_rand, delta_q/dist)
+        dist = delta_q
+    dt = dist / discretisationsteps
+    for i in range(1, discretisationsteps):
+        q_lerp = lerp(q_near, q_end, (dt*i)/dist)
+        if cube_collision(pin.SE3(q_lerp)):
+            return lerp(q_near, q_end, (dt*(i-1))/dist)
+    return q_end
+
+def NEW_CONF_ROBOT(q_near, q_rand, discretisationsteps, delta_q=None):
+    q_end = q_rand.copy()
+    dist = distance_robot(q_near, q_rand)
+    if delta_q is not None and dist > delta_q:
+        q_end = lerp(q_near, q_rand, delta_q/dist)
+        dist = delta_q
+    dt = dist / discretisationsteps
+    for i in range(1, discretisationsteps):
+        q_lerp = lerp(q_near, q_end, (dt*i)/dist)
+        if robot_collision(q_lerp):
+            return lerp(q_near, q_end, (dt*(i-1))/dist)
+    return q_end
+
+def VALID_EDGE(q_new, q_goal, discretisationsteps):
+    return np.linalg.norm(q_goal - NEW_CONF_ROBOT(q_new, q_goal, discretisationsteps)) < 1e-3
+
+def getpath(G):
+    path = []
+    node = G[-1]
+    while node[0] is not None:
+        path = [node[1]] + path
+        node = G[node[0]]
+    path = [G[0][1]] + path
+    return path
 
 
 #returns a collision free path from qinit to qgoal under grasping constraints
@@ -22,14 +108,11 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
     discretisationsteps_newconf = 200 
     discretisationsteps_validedge = 200 
     k = 1000
-    delta_q = 3.
-    min_dist = 10e4
-    idx = -1
+    delta_q = 3.0
     
-    G = [(None,np.array(qinit))]
+    G = [(None,np.array(cubeplacementq0), np.array(qinit))]
     rotation = cubeplacementq0.rotation
     sampled_positions = set()
-    goal_bias = 0.1
 
     x_range = (0, 0.5) 
     y_range = (-0.2, 0.2)
@@ -57,74 +140,37 @@ def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal):
                 if not success:
                     break
                 else: 
-                    
                     # Change the range of the sampling space 
                     x_range = (min(0, cube_x_rand - 0.1), max(0.5, cube_x_rand + 0.1))
                     y_range = (min(-0.2, cube_y_rand - 0.1), max(0.2, cube_y_rand + 0.1))   
                     z_range = (min(0.93, cube_z_rand - 0.1), max(1.2, cube_z_rand + 0.1))
         
-        # Find the nearest vertex to q_rand called q_near 
-        for i, (parent,node) in enumerate(G):
-            dist = np.linalg.norm(q_rand - node[1])
-            if dist < min_dist: 
-                min_dist = dist
-                idx = i
-        q_near_index = idx
-        q_near = G[q_near_index][1]
+
+        cube_q_near_idx = NEAREST_VERTEX_CUBE_Q(G, cube_q_rand)
+        cube_q_near = G[cube_q_near_idx][1]
+        cube_q_near = pin.SE3(cube_q_near)
         
-        print(f"Nearest vertex: {q_near}")
+        q_near_idx = NEAREST_VERTEX_ROBOT_Q(G, q_rand)
+        q_near, success = computeqgrasppose(robot, q_rand, cube, cube_q_near, viz)
+        print(f"Nearest vertex: {cube_q_near}")
 
     # Return the closest configuration q_new such that the path q_near => q_new is the longest 
     # along the linear interpolation (q_near,q_rand) that is collision free and of length <  delta_q
-        q_end = q_rand.copy()
-        dist_two = np.linalg.norm(q_rand - q_near)
-        if delta_q is not None and dist_two > delta_q:
-            q_end = q_near * (1 - delta_q / dist) + q_rand * (delta_q / dist_two)
-            dist_two = delta_q  
-        dt = dist_two / discretisationsteps_newconf
-        for i in range(1, discretisationsteps_newconf):
-            q_new = q_near * (1 - dt*i) + q_end * (dt*i)
-            q_newt, success = computeqgrasppose(robot, qinit, cube, cube_q_rand, viz)
-            if success:
-                q_new = q_near * (1 - dt*(i-1)) + q_end * (dt*(i-1))
-            else:
-                q_new = q_end
-        q_new = np.array(q_new)
-        print(f"New configuration: {q_new}")
-    # Add the edge and vertex from q_near to q_new to the tree G
-        G += [(q_near_index, q_new)]
+        cube_q_new = NEW_CONF_CUBE(cube_q_near, cube_q_rand, discretisationsteps_newconf, delta_q)
+        print(f"New cube configuration: {cube_q_new}")
+        cube_q_new = pin.SE3(cube_q_new)
+
+        q_new, success = computeqgrasppose(robot, q_near, cube, cube_q_new, viz)
 
     # Return the closest configuration q such that the path q => q_new is the longest 
     # along the linear interpolation (q_new,qgoal) that is collision free and of length <  delta_q
-        q_end_two = qgoal.copy()
-        dist_three = np.linalg.norm(qgoal - q_new) 
-        if delta_q is not None and dist_three > delta_q:
-            q_end_two = q_new * (1 - delta_q / dist_three) + qgoal * (delta_q / dist_three)
-            dist_three = delta_q
-        dt = dist_three / discretisationsteps_validedge
-        for i in range(1, discretisationsteps_validedge):
-            q = q_new * (1 - dt*i) + q_end_two * (dt*i)
-            q1, success_two = computeqgrasppose(robot, qinit, cube, cube_q_rand, viz)
-            if success_two:
-                q = q_new * (1 - dt*(i-1)) + q_end_two * (dt*(i-1))
-            else:
-                q = q_end_two
-        q = np.array(q)
-        print(f"New configuration: {q}")
-        # If the edge between q_new and q_goal is valid then a path has been found 
-        if np.linalg.norm(qgoal - q) < 1e-3:
+        ADD_EDGE_AND_VERTEX(G, q_near_idx, np.array(cube_q_new), np.array(q_new))
+        if VALID_EDGE(q_new, qgoal, discretisationsteps_validedge):
             print("Path found!")
-            G += [(len(G)-1, np.array(qgoal))]
-            break
+            ADD_EDGE_AND_VERTEX(G, len(G)-1, np.array(cubeplacementqgoal), np.array(qgoal))
     
     # Reconstruct the path from qinit to qgoal
-    path = []
-    node = G[-1]
-    while node[0] is not None:
-        path = [node[1]] + path
-        node = G[node[0]]
-    path = [G[0][1]] + path
-    print(path)
+    path = getpath(G)
     return path
                                              
 
